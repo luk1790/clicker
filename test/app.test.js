3393414337548c7623ac18a7c46356be4eff2b0e
Lukasz Rak <luk1790@gmail.com>
2018-08-07 15:16:22 +0200

    chore
    add tests

---
+++ test/app.test.js
@@ -0,0 +1,9 @@
+var chai = require('chai');  
+var assert = chai.assert;
+var expect = chai.expect;
+var should = chai.should();
+describe('test', function() {
+    it('test funcionality', function(){
+        assert.equal('test', 'test');
+    })
+  });
